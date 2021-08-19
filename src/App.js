a1da661bbe640cf63feac3b61cebeceeea26699d
Upal Patel <patelupal98@gmail.com>
2021-08-18 20:28:07 -0700

    implemented useState and events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
@@ -28,9 +31,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
