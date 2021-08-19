228e67f183e9252c7ea4d772d66323550d7fa579
Upal Patel <patelupal98@gmail.com>
2021-08-18 21:52:49 -0700

    added a chart

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Sony A7III",
+    amount: 1699.99,
+    date: new Date(2019, 11, 16),
+  },
+  {
+    id: "e2",
+    title: "Tamron 28mm-75mm f2.8 Lens",
+    amount: 879.0,
+    date: new Date(2019, 11, 16),
+  },
+  {
+    id: "e3",
+    title: "DJI Mavic 2 Pro",
+    amount: 1599.0,
+    date: new Date(2019, 2, 15),
+  },
+  {
+    id: "e4",
+    title: "Zhiyun Crane 2S",
+    amount: 599.0,
+    date: new Date(2020, 9, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Sony A7III",
-      amount: 1699.99,
-      date: new Date(2019, 8, 16),
-    },
-    {
-      id: "e2",
-      title: "Tamron 28mm-75mm f2.8 Lens",
-      amount: 879.0,
-      date: new Date(2019, 8, 16),
-    },
-    {
-      id: "e3",
-      title: "DJI Mavic 2 Pro",
-      amount: 1599.0,
-      date: new Date(2018, 12, 15),
-    },
-    {
-      id: "e4",
-      title: "Zhiyun Crane 2S",
-      amount: 599.0,
-      date: new Date(2020, 6, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -51,7 +51,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -94,6 +94,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
